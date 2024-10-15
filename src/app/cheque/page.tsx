ab198d6e4c9c670d488a16992ba017862adbd24b
jenerfan <jenerfan@hotmail.com>
2024-10-15 11:00:15 +0800

    update  .evn

--- src/app/cheque/page.tsx
+++ src/app/cheque/page.tsx
@@ -18,7 +18,7 @@ const FormElementsPage = () => {
       formData.append("files", images[i]);
     }
     try {
-      const response = await fetch("http://localhost:9090/api/pdf/generate", {
+      const response = await fetch(`${process.env.BASE_URL}/api/pdf/generate`, {
         method: "POST",
         body: formData,
       });
